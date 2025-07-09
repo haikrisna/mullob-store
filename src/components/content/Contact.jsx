6488f3d85dcb8939523cdead4d1d5fa36b890129
Krisna Kukuh Wijaya <krisnakukuh54@gmail.com>
2025-07-09 09:17:48 +0700

    remove noused component

--- src/components/content/Contact.jsx
+++ src/components/content/Contact.jsx
@@ -6,21 +6,6 @@ import {
   WhatsappLogoIcon,
 } from "@phosphor-icons/react";
 
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-
 export default function ContactWithMap() {
   const socialMedia = [
     {
